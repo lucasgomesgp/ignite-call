145848a3d91815685b7dc847478ac5b8bec44177
Lucas Gomes <lucasgomesbt@gmail.com>
2025-03-05 23:03:12 -0300

    feat: initial structure with - home, register and connect calendar

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  pageExtensions:[
+    "page.tsx",
+    "api.ts",
+    "api.tsx"
+  ]
+};
+
+export default nextConfig;
