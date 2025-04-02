fa79fde56280f28c113f66331ab09acdb7348b77
Lucas Gomes <lucasgomesbt@gmail.com>
2025-04-02 18:40:15 -0300

    feat: prisma adapter all methods

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,11 +1,15 @@
-import NextAut from "next-auth"
+import NextAuth from 'next-auth'
 
-declare module "next-auth" {
-    interface User {
+declare module 'next-auth' {
+    export interface User {
         id: string
         name: string
         email: string
         username: string
         avatar_url: string
+    }
+
+    interface Session {
+        user: User
     }
 }
--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,8 +1,10 @@
+import { PrismaAdapter } from "@/src/lib/auth/prisma-adapter"
 import NextAuth, { type NextAuthOptions } from "next-auth"
 
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
